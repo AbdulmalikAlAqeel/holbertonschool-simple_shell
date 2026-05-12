a1d62bb50d46c49cade4bfb3b3ff102d5f8aa5c7
MN7 <you@example.com>
2026-05-12 13:03:58 +0300

    simple shell program

---
+++ simple-shell.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void)
+{
+pid_t my_pid;
+int error;
+int status, i = 0;
+ssize_t read;
+char *argv[64];
+char *token;
+char *line = NULL;
+size_t len = 0;
+while (true)
+{
+printf("$ ");   
+read = getline(&line, &len, stdin);
+if (read == -1)
+{
+printf("\n");
+break;
+}
+my_pid = fork();
+if (my_pid == 0)
+{
+line[strcspn(line, "\n")] = '\0';
+token = strtok(line, " ");
+while (token)
+{
+    argv[i++] = token;
+    token = strtok(NULL, " ");
+}
+argv[i] = NULL;
+if (strlen(line) > 1)
+{
+error = execve(argv[0], argv, NULL);
+if (error == -1)
+{
+printf("./shell: No such file or directory\n");
+}
+}
+break;
+}
+else
+{
+wait(NULL);
+}
+
+}
+return (0);
+}
