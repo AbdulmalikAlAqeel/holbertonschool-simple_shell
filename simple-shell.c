66dc08fd8df3836e9893afb604d71d139a99652c
Your Name <you@example.com>
2026-05-13 16:54:17 +0300

    The last for this task

--- simple-shell.c
+++ simple-shell.c
@@ -7,92 +7,110 @@
 
 char *find_in_path(char *cmd);
 
-/**
- * main - entry point
- * Return: always (0) success
- */
-int main()
+int main(int argc, char **argv_main, char **envp)
 {
-pid_t pid;
-ssize_t read;
-size_t len = 0;
-char *argv[64];
-char *token;
-int status;
-int i;
-int exit_status = 0;
-char *line = NULL;
+    pid_t pid;
+    ssize_t read;
+    size_t len = 0;
+    char *argv[64];
+    char *token;
+    int status;
+    int i;
+    int exit_status = 0;
+    char *line = NULL;
+    char *cmd_path;
 
+    (void)argc;
+    (void)argv_main;
 
-while (1)
-{
+    while (1)
+    {
+        i = 0;
 
-i = 0;
-if (isatty(STDIN_FILENO))
-{
-printf("$ ");
-}
-read = getline(&line, &len, stdin);
-if (read == -1)
-{
-if (isatty(STDIN_FILENO))
-{
-printf("\n");
-}
-break;
-}
-line[strcspn(line, "\n")] = '\0';
-token = strtok(line, " ");
-while (token)
-{
-argv[i++] = token;
-token = strtok(NULL, " ");
-}
-argv[i] = NULL;
-if (argv[0] == NULL)
-{
-continue;
-}
-pid = fork();
-if (pid == 0)
-{
-if (argv[0][0] != '/' && argv[0][0] != '.')
-{
-argv[0] = find_in_path(argv[0]);
-}
-execve(argv[0], argv, NULL);
-free(line);
-exit(127);
-}
-else
-{
-wait(&status);
-if (WIFEXITED(status))
-    exit_status = WEXITSTATUS(status);
-}
-}
-free(line);
-return (exit_status);
-}
+        if (isatty(STDIN_FILENO))
+            printf("$ ");
 
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            if (isatty(STDIN_FILENO))
+                printf("\n");
+            break;
+        }
+
+        line[strcspn(line, "\n")] = '\0';
+
+        token = strtok(line, " ");
+        while (token)
+        {
+            argv[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        argv[i] = NULL;
+
+        if (argv[0] == NULL)
+            continue;
+
+        if (strchr(argv[0], '/'))
+        {
+            cmd_path = argv[0];
+        }
+        else
+        {
+            cmd_path = find_in_path(argv[0]);
+            if (cmd_path == NULL)
+            {
+                exit_status = 127;
+                continue;
+            }
+            argv[0] = cmd_path;
+        }
+
+        pid = fork();
+        if (pid == 0)
+        {
+            execve(argv[0], argv, envp);
+            exit(127);
+        }
+        else
+        {
+            wait(&status);
+            if (WIFEXITED(status))
+                exit_status = WEXITSTATUS(status);
+        }
+
+        if (!strchr(argv[0], '/') && cmd_path != NULL)
+            free(cmd_path);
+    }
+
+    free(line);
+    return (exit_status);
+}
 
 char *find_in_path(char *cmd)
 {
-    char *path = getenv("PATH");
-    char *copy = strdup(path);
-    char *dir = strtok(copy, ":");
+    char *path;
+    char *copy;
+    char *dir;
     char full[1024];
 
+    path = getenv("PATH");
+    if (path == NULL || path[0] == '\0')
+        return NULL;
+
+    copy = strdup(path);
+    if (!copy)
+        return NULL;
+
+    dir = strtok(copy, ":");
     while (dir)
     {
         snprintf(full, sizeof(full), "%s/%s", dir, cmd);
-
         if (access(full, X_OK) == 0)
         {
             free(copy);
             return strdup(full);
         }
-
         dir = strtok(NULL, ":");
     }
 
